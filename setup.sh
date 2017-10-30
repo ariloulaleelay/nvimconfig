e83559b544b77fae728abddcc94801043dd7dc16
Andrey Proskurnev <a.proskurnev@ntechlab.com>
2017-10-30 19:18:51 +0300

    Add powerline fonts to setup

--- setup.sh
+++ setup.sh
@@ -14,3 +14,11 @@ cd $DIR/bundle && git clone https://github.com/VundleVim/Vundle.vim.git
 # TODO
 # install fonts
 # https://github.com/powerline/fonts
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
